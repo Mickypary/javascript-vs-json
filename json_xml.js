8e44e501a63a5a56d853ccb25d0c6d0ecaa07fe2
Mickypary <mikipary@gmail.com>
2026-09-06 15:20:23 +0100

    Another Commit

---
+++ json_xml.js
@@ -0,0 +1,20 @@
+// JSON VS XML
+
+`{ 
+    "animals" : [
+      {"type": "Dog","sound": "woof"},
+      {"type": "Cat","sound": "meow"}
+]}`;
+
+// XML
+
+<animals>
+  <animal>
+    <type>Dog</type>
+    <sound>Woof</sound>
+  </animal>
+  <animal>
+    <type>Cat</type>
+    <sound>Meow</sound>
+  </animal>
+</animals>;
